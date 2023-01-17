00efc99034a97f90e0f051f7cb0debe4488c5e9f
MarcellusCodes <MarcellusCodes@gmail.com>
2023-01-17 21:14:52 +0100

    added global product type, slug page for product, product query

---
+++ pages/products/[slug].tsx
@@ -0,0 +1,61 @@
+import Card from "@/components/home/card";
+import Layout from "@/components/layout";
+import Balancer from "react-wrap-balancer";
+import Link from "next/link";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { getProducts, getProduct } from "@/api/queries";
+import { Puzzle, PackageCheck } from "lucide-react";
+import type { Product } from "@/types/index";
+
+const Product: NextPage<{ product: Product }> = ({ product }) => {
+  return (
+    <Layout>
+      <div className=" grid grid-cols-4 gap-8">
+        <article
+          key={product.id}
+          className="flex w-full flex-col items-start gap-4 rounded-md border border-slate-900 border-opacity-25 bg-slate-100 bg-opacity-20 p-4 transition-all duration-300 hover:bg-opacity-10"
+        >
+          <h3 className="w-full text-3xl font-bold text-slate-900">
+            <Balancer>{product.name}</Balancer>
+          </h3>
+          <p className="text-slate-900 text-opacity-80">
+            <Balancer>{product.description}</Balancer>
+          </p>
+          <ul>
+            <li className="text-xl font-bold">{product.price}€</li>
+            <li className="flex flex-row items-center">
+              <Puzzle color="black" size={16} /> {product.pieces} left
+            </li>
+            <li className="flex flex-row items-center">
+              <PackageCheck color="black" size={16} /> {product.status}
+            </li>
+          </ul>
+          <button className="rounded-sm bg-slate-50 py-2 px-8">
+            Add to cart
+          </button>
+        </article>
+      </div>
+    </Layout>
+  );
+};
+
+export default Product;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const productSlug = context.params?.slug as string;
+  const product = await getProduct(productSlug);
+
+  return {
+    props: { product: product.data[0], key: product.data[0].id },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const products = await getProducts();
+
+  const paths = products.data.map((product: { slug: string }) => ({
+    params: { slug: product.slug },
+  }));
+
+  return { paths, fallback: "blocking" };
+};
