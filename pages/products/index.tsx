f293c7bb42de18fd32e750ae361f1fe1e393274b
MarcellusCodes <MarcellusCodes@gmail.com>
2023-01-23 21:36:37 +0100

    Fixed Linting and and type checking

---
+++ pages/products/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout";
+import { NextPage } from "next";
+
+const Products: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Products</h1>
+    </Layout>
+  );
+};
+
+export default Products;
